a91c6b459053de3fd9f056690fdfffc44e6a737b
Anastasia <stefanenkova.anastasia.w@gmail.com>
2024-02-09 17:30:46 +0300

    2 les

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MainController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+Route::get('/articles', [MainController::class, 'index']);
+Route::get('/full-img/{img}', [MainController::class, 'show']);
 
 Route::get('/', function () {
     return view('layout');
